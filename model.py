c533f29ab05d85f1723c0ceed144b50ebae641fd
Om-Alve <omalve8174@gmail.com>
2024-07-20 19:10:59 +0530

    Densenet

--- model.py
+++ model.py
@@ -2,54 +2,106 @@ import torch
 import torch.nn as nn
 from config import DROPOUT
 
-class Classifier(nn.Module):
-    def __init__(self,inchannels :int = 1,num_classes :int = 10):
+class TransitionLayer(nn.Module):
+    def __init__(self,num_features,reduction):
         super().__init__()
-        self.convnet = nn.Sequential(
-            nn.Conv2d(in_channels=inchannels,out_channels=32,kernel_size=3),
-            nn.BatchNorm2d(32),
-            nn.LeakyReLU(0.2),
-            nn.Dropout2d(DROPOUT),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-            
-            nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3),
-            nn.BatchNorm2d(64),
-            nn.LeakyReLU(0.2),
-            nn.Dropout2d(DROPOUT),
-            nn.MaxPool2d(kernel_size=(2, 2), stride=(2, 2)),
-
-            nn.Conv2d(in_channels=64,out_channels=128,kernel_size=4),
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2),
-            nn.Dropout2d(DROPOUT),
-            nn.MaxPool2d(kernel_size=4, stride=4),
-            nn.Flatten(),
-            nn.Linear(512,256),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(DROPOUT),
-            nn.Linear(256,128),
-            nn.LeakyReLU(0.2),
-            nn.Dropout(DROPOUT),
-            nn.Linear(128,num_classes),
+        self.conv = nn.Sequential(
+            nn.BatchNorm2d(num_features),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(num_features,int(num_features * reduction),kernel_size=1,stride=1,bias=False),
+            nn.AvgPool2d(kernel_size=2,stride=2),
         )
+    def forward(self,x):
+        return self.conv(x)
     
-    def forward(self,x : torch.Tensor) -> torch.Tensor:
-        return self.convnet(x)
+class DenseLayer(nn.Module):
+    def __init__(self,num_features,growth_rate,bottleneck_size,dropout=0.2):
+        super().__init__()
+        self.bottleneck = nn.Sequential(
+            nn.BatchNorm2d(num_features),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(num_features,bottleneck_size * growth_rate,kernel_size=1,stride=1,bias=False),
+        ) 
+        self.conv = nn.Sequential(
+            nn.BatchNorm2d(bottleneck_size * growth_rate),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(bottleneck_size * growth_rate, growth_rate,kernel_size=3,stride=1,padding=1,bias=False),
+            nn.Dropout(dropout),
+        )
+        
+    def forward(self,input):
+        prev_features = input
+        prev_features = torch.cat(prev_features,dim=1)
+        bottleneck_out = self.bottleneck(prev_features)
+        out_features = self.conv(bottleneck_out)
+        
+        return out_features
+
+class DenseBlock(nn.Module):
+    def __init__(self,num_layers,num_features,bottleneck_size,growth_rate):
+        super().__init__()
+        self.layers = nn.ModuleList([DenseLayer(num_features + i * growth_rate,growth_rate,bottleneck_size) for i in range(num_layers)])
     
+    def forward(self,init_features):
+        features = [init_features]
+        for layer in self.layers:
+            new_features = layer(features)
+            features.append(new_features)
+        return torch.cat(features,dim=1)
+    
+class DenseNet(nn.Module):
+    def __init__(self, num_init_features, bottleneck_size, growth_rate, reduction=0.5, blocks=[6, 12, 24, 16], num_classes=10):
+        super().__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, num_init_features, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(num_init_features),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
+
+        num_features = num_init_features
+        self.blocks = nn.ModuleList()
+        self.transitions = nn.ModuleList()
+
+        for i, num_layers in enumerate(blocks):
+            block = DenseBlock(num_layers, num_features, bottleneck_size, growth_rate)
+            self.blocks.append(block)
+            num_features += num_layers * growth_rate
+            if i != len(blocks) - 1:
+                transition = TransitionLayer(num_features, reduction)
+                self.transitions.append(transition)
+                num_features = int(num_features * reduction)
+        self.final_norm = nn.BatchNorm2d(num_features)
+        self.pool = nn.AvgPool2d(4)
+        self.head = nn.Linear(num_features, num_classes)
+        self._initialize_weights()
+
     def _initialize_weights(self):
         for m in self.modules():
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-                torch.nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='leaky_relu')
-                if m.bias is not None:
-                    torch.nn.init.constant_(m.bias, 0)
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='linear')
+                nn.init.constant_(m.bias, 0)
             elif isinstance(m, nn.BatchNorm2d):
-                torch.nn.init.constant_(m.weight, 1)
-                torch.nn.init.constant_(m.bias, 0)
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+
+    def forward(self, x):
+        x = self.features(x)
+        for i, block in enumerate(self.blocks):
+            x = block(x)
+            if i < len(self.transitions):
+                x = self.transitions[i](x)
+        x = self.pool(self.final_norm(x))
+        x = torch.flatten(x, 1)
+        x = self.head(x)
+        return x
 
 
 if __name__ == '__main__':
-    x = torch.randn(1,1,64,64)
-    model = Classifier()
+    x = torch.randn(2,1,64,64)
+    model = DenseNet(num_init_features=64,bottleneck_size=2,growth_rate=12)
     params = sum([p.numel() for p in model.parameters()])
     print(f"Params: {params}")
     print(model(x).shape)
--- train.py
+++ train.py
@@ -5,12 +5,12 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST,FashionMNIST
 from utils import train_transforms,calculate_accuracy_and_loss,test_transforms
 from config import *
-from model import Classifier
+from model import DenseNet
 from tqdm import tqdm
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
 
-torch.backends.cudnn.benchmark = True
+# torch.backends.cudnn.benchmark = True
 torch.manual_seed(1)
 torch.cuda.manual_seed(1)
 
@@ -19,11 +19,11 @@ if __name__ == '__main__':
     test_dataset = FashionMNIST(root='~/datasets',train=False,transform=test_transforms,download=True)
     train_dataloader = DataLoader(train_dataset,batch_size=BATCH_SIZE,shuffle=True,num_workers=4)
     test_dataloader = DataLoader(test_dataset,batch_size=BATCH_SIZE,shuffle=False,num_workers=2)
-    model = Classifier().to(DEVICE)
+    model = DenseNet(num_init_features=64,bottleneck_size=2,growth_rate=12).to(DEVICE)
     opt = torch.optim.AdamW(lr=LEARNING_RATE,params=model.parameters())
-    scheduler = torch.optim.lr_scheduler.OneCycleLR(opt,max_lr=1e-2,total_steps=NUM_EPOCHS * len(train_dataloader),anneal_strategy='cos',max_momentum=0.90)
-
-    writer = SummaryWriter("runs/fashion_MNIST_v8.0_SuperConvergence")
+    # scheduler = torch.optim.lr_scheduler.OneCycleLR(opt,max_lr=1e-2,total_steps=NUM_EPOCHS * len(train_dataloader),anneal_strategy='cos',max_momentum=0.90)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(opt,T_max=NUM_EPOCHS * len(train_dataloader),eta_min=1e-4)
+    writer = SummaryWriter("runs/fashion_MNIST_DENSENET")
     for i in range(NUM_EPOCHS):
         loader = tqdm(train_dataloader,leave=False)
         loader.set_description(f"Epoch: {i}")
